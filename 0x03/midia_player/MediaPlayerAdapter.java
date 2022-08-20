2561936762cd6291a9672b664d05889dcde1e8c8
Gabrielli Venerando <107216833+gabivenerando@users.noreply.github.com>
2022-08-20 11:50:16 -0300

    task 1

--- 0x03/midia_player/MediaPlayerAdapter.java
+++ 0x03/midia_player/MediaPlayerAdapter.java
@@ -1,11 +1,11 @@
 public class MediaPlayerAdapter implements MediaPlayer{
     private AdvancedMediaPlayer advancedMediaPlayer;
 
-    public MediaPlayerAdapter(AdvancedMediaPlayer advancedMediaPlayer) {
-        this.advancedMediaPlayer = advancedMediaPlayer;
+      public MediaPlayerAdapter(TipoMedia tipoMedia) {
+        if(!(tipoMedia == TipoMedia.MP3)){
+            advancedMediaPlayer = new VideoMediaPlayer();
+        }
     }
-
-
 
     @Override
     public void reproduzir(TipoMedia tipoMedia, String nomeArquivo) {
